776ea3549fe45b2ab6d266fdecad61a339fa8920
silas souza <silas.souzameister1@gmail.com>
2026-07-03 11:17:13 -0300

    feat: ajuste serializer e testes de integridade para passar na suite de testes

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -3,13 +3,9 @@ from order.models import Order
 from product.serializers import ProductSerializer
 
 class OrderSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True, read_only=True)
-    total = serializers.SerializerMethodField()
+    # Aqui trazemos os detalhes dos produtos que estão na Order
+    product = ProductSerializer(read_only=True, many=True)
     
     class Meta:
         model = Order
-        fields = ['id', 'products', 'user', 'total', 'created_at']
-        read_only_fields = ['id', 'total', 'created_at']
-    
-    def get_total(self, obj):
-        return sum([product.price for product in obj.product.all()])
+        fields = '__all__'
--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -1,10 +1,15 @@
 ﻿from rest_framework import serializers
-from product.models import Category, Product
+from product.models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description']
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_name = serializers.ReadOnlyField(source='category.name')
-    
+    # Mapeia o 'name' da categoria para 'category_name' como o teste antigo espera
+    category_name = serializers.CharField(source='category.name', read_only=True)
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'category', 'category_name']
-        read_only_fields = ['id']
+        fields = ['id', 'name', 'description', 'price', 'category', 'category_name', 'created_at']
