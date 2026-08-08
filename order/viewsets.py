889cd9f52c69295048d04809843a04c499e767f8
silas souza <silas.souzameister1@gmail.com>
2026-08-08 18:19:48 -0300

    fix: protege rota de pedidos com autenticação

--- order/viewsets.py
+++ order/viewsets.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from .models import Order
 from .serializers import OrderSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
