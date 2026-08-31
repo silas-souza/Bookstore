4543002e823c8811163104b8204393d05d172fbf
silas souza <silas.souzameister1@gmail.com>
2026-08-31 19:13:07 -0300

    fix: restore product and order apps

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "rest_framework.authtoken",
+    "product",
+    "order",
 ]
 
 MIDDLEWARE = [
